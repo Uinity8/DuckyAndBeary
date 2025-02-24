6363c709cd2aaa274e812a489a77f8910cdedad1
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-02-24 11:49:08 +0900

    [fix] Camera 적용 및 오류 수정
    - SampleScene에서 카메라 오류 수정 및 적용

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,79 +4,82 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+namespace Entity
 {
-    PlayerController[] players;
-    Camera camera;
-
-    [Header("카메라 이동 가능 범위")]
-    [SerializeField] Rect boundary;
-    [SerializeField] Color gizmoColor;
-
-    [Header("카메라 줌 인&아웃 조절")]
-    [SerializeField] float distanceWeight;
-    [SerializeField] float minValue;
-
-    private const float cameraZPosition = -10f;
-
-    void Start()
+    public class CameraController : MonoBehaviour
     {
-        players = FindObjectsOfType<PlayerController>();
-        camera = Camera.main;
-    }
+        PlayerController[] players;
+        Camera mainCamera;
 
-    private void Update()
-    {
-        Vector2 middlePoint = CalculatePlayersCenter();
-        MoveCamera(middlePoint);
-        ZoomInOrOut(CalculateMaxPlayerDistance());
-    }
+        [Header("카메라 이동 가능 범위")]
+        [SerializeField] Rect boundary;
+        [SerializeField] Color gizmoColor;
 
-    void OnDrawGizmosSelected()
-    {
-        if (boundary == default) return;
+        [Header("카메라 줌 인&아웃 조절")]
+        [SerializeField] float distanceWeight;
+        [SerializeField] float minValue;
 
-        Gizmos.color = gizmoColor;
-        Vector3 center = new Vector3(boundary.x + boundary.width / 2, boundary.y + boundary.height / 2);
-        Vector3 size = new Vector3(boundary.width, boundary.height);
-        Gizmos.DrawCube(center, size);
-    }
+        private const float cameraZPosition = -10f;
 
-    void MoveCamera(Vector2 middlePoint)
-    {
-        Vector3 targetPos = new Vector3(
-            Mathf.Clamp(middlePoint.x, boundary.xMin, boundary.xMax),
-            Mathf.Clamp(middlePoint.y, boundary.yMin, boundary.yMax),
-            cameraZPosition
-        );
-
-        transform.position = Vector2.Lerp(targetPos, transform.position, 0f);
-    }
-
-    Vector2 CalculatePlayersCenter()
-    {
-        Vector2 center = players.Aggregate(Vector2.zero, (sum, player) => sum + (Vector2)player.transform.position);
-        return center / players.Length;
-    }
-
-    float CalculateMaxPlayerDistance()
-    {
-        float maxDistance = 0;
-
-        for (int i = 0; i < players.Length; i++)
+        void Start()
         {
-            for (int j = i + 1; j < players.Length; j++)
-            {
-                float tempDistance = Vector2.Distance(players[i].transform.position, players[j].transform.position);
-                maxDistance = Mathf.Max(maxDistance, tempDistance);
-            }
+            players = FindObjectsOfType<PlayerController>();
+            mainCamera = GetComponent<Camera>();
         }
 
-        return maxDistance;
-    }
+        private void Update()
+        {
+            Vector2 middlePoint = CalculatePlayersCenter();
+            MoveCamera(middlePoint);
+            ZoomInOrOut(CalculateMaxPlayerDistance());
+        }
 
-    void ZoomInOrOut(float distance)
-    {
-        camera.orthographicSize = minValue + distanceWeight * Mathf.Sqrt(distance);
+        void OnDrawGizmosSelected()
+        {
+            if (boundary == default) return;
+
+            Gizmos.color = gizmoColor;
+            Vector3 center = new Vector3(boundary.x + boundary.width / 2, boundary.y + boundary.height / 2);
+            Vector3 size = new Vector3(boundary.width, boundary.height);
+            Gizmos.DrawCube(center, size);
+        }
+
+        void MoveCamera(Vector2 middlePoint)
+        {
+            Vector3 targetPos = new Vector3(
+                Mathf.Clamp(middlePoint.x, boundary.xMin, boundary.xMax),
+                Mathf.Clamp(middlePoint.y, boundary.yMin, boundary.yMax),
+                cameraZPosition
+            );
+
+            transform.position = targetPos;
+        }
+
+        Vector2 CalculatePlayersCenter()
+        {
+            Vector2 center = players.Aggregate(Vector2.zero, (sum, player) => sum + (Vector2)player.transform.position);
+            return center / players.Length;
+        }
+
+        float CalculateMaxPlayerDistance()
+        {
+            float maxDistance = 0;
+
+            for (int i = 0; i < players.Length; i++)
+            {
+                for (int j = i + 1; j < players.Length; j++)
+                {
+                    float tempDistance = Vector2.Distance(players[i].transform.position, players[j].transform.position);
+                    maxDistance = Mathf.Max(maxDistance, tempDistance);
+                }
+            }
+
+            return maxDistance;
+        }
+
+        void ZoomInOrOut(float distance)
+        {
+            mainCamera.orthographicSize = minValue + distanceWeight * Mathf.Sqrt(distance);
+        }
     }
 }
