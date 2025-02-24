173819c049828ecd149f0622e3a50bbf000020ac
allhoon18 <83628265+allhoon18@users.noreply.github.com>
2025-02-24 21:06:37 +0900

    [fix] Camera 로직 수정

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -18,23 +18,44 @@ namespace Entity
         [SerializeField] Color gizmoColor;
 
         [Header("카메라 줌 인&아웃 조절")]
-        [SerializeField] float distanceWeight;
+        [SerializeField] float cameraMargin;
         [SerializeField] float minValue;
 
         private const float cameraZPosition = -10f;
+
+        [SerializeField] float startCameraSize;
+        [SerializeField] Vector3 startCameraPos;
+        Vector3 readyPos = new Vector3(0f, 1f, -15f);
+        float readySize = 6;
+
+        float t = 0f;
 
         void Start()
         {
             players = FindObjectsOfType<PlayerController>();
             mainCamera = GetComponent<Camera>();
             pixelPerfectCamera = GetComponent<PixelPerfectCamera>();
+
+            mainCamera.orthographicSize = startCameraSize;
+            mainCamera.transform.position = startCameraPos;
         }
 
         private void Update()
         {
+            
+            t+= Time.deltaTime;
+            transform.position = Vector3.Lerp(startCameraPos, readyPos, t/3f);
+            mainCamera.orthographicSize = Mathf.Lerp(startCameraSize, readySize, t/3f);
+
+            //Invoke("UpdateCamera", 3f);
+        }
+
+        void UpdateCamera()
+        {
             Vector2 middlePoint = CalculatePlayersCenter();
             MoveCamera(middlePoint);
             ZoomInOrOut(CalculateMaxPlayerDistance());
+
         }
 
         void OnDrawGizmosSelected()
@@ -72,7 +93,7 @@ namespace Entity
             {
                 for (int j = i + 1; j < players.Length; j++)
                 {
-                    float tempDistance = Vector2.Distance(players[i].transform.position, players[j].transform.position);
+                    float tempDistance = Mathf.Abs(players[i].transform.position.x - players[j].transform.position.x);
                     maxDistance = Mathf.Max(maxDistance, tempDistance);
                 }
             }
@@ -84,7 +105,14 @@ namespace Entity
         {
             pixelPerfectCamera.CorrectCinemachineOrthoSize(0);
 
-            mainCamera.orthographicSize = minValue + distanceWeight * distance;
+            float initCameraWidth = minValue * 3;
+
+            float value = initCameraWidth
+                + Mathf.Max(0, distance - (initCameraWidth - cameraMargin*2));
+
+            value /= 3f;
+
+            mainCamera.orthographicSize = value;
         }
     }
 }
