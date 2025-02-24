8176f42049e14da05f8ea65b4f7c0fb195d2ee62
AAAAAA123222 <kdo03122@gmail.com>
2025-02-24 19:24:12 +0900

    feat: 파티클 작성, 보석 스프라이트 추가 및 물 스크립트 편집 (#17)
    * feat: ObjectIdentifier 작업
    
    * feat: ObjectIdentifier, ItemController 코드 작성
    
    ObjectIdentifier와 ItemController 코드를 작성했습니다.
    
    * feat: 플레이어의 레이어 분리
    
    Player 단일로 작성되어있던 레이어를 Ducky와 Beary로 구분했습니다.
    
    * feat: utf-8 서식 수정, Gem 타겟 설정
    
    * feat: 물 타일 추가 및 문 오브젝트, Fluid, ExitController 스크립트 작성
    
    * feat: 머지를 대비해서 이름 변경
    
    * feat: 파티클 작성, 보석 스프라이트 추가 및 물 스크립트 작성
    
    * Create SampleScene.unity.meta

--- Assets/Scripts/Fluid.cs
+++ Assets/Scripts/Fluid.cs
@@ -10,24 +10,19 @@ public class Fluid : ObjectIdentifier
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision != null)
-        {
-            splashEffect.transform.position = collision.transform.position; // 충돌 위치로 이동
-            splashEffect.Play(); // 물 튀기는 이펙트 실행
-        }
-        
+        splashEffect.transform.position = collision.transform.position; // 충돌 위치로 이동
+        splashEffect.Play(); // 물 튀기는 이펙트 실행
         if (LayerCheck(collision.gameObject.layer))
         {
-            StatHandler bear = collision.GetComponent<StatHandler>();
+            PlayerController bear = collision.GetComponent<PlayerController>();
 
             if (bear != null)
             {
                 Debug.Log($"{collision.gameObject.name}이 물에 닿았습니다.");
-
-                SignalManager.Instance.EmitSignal(GameManager.GameOverkey);
                 //bear.Death() //플레이어 컴포넌트 안에 있는 사망 메서드 호출
                 //바다에 빠진 파티클 이펙트 재생
             }
         }
     }
 }
+
