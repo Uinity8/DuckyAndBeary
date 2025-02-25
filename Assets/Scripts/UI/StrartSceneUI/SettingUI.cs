0ba53b8bd82d65d35fa22f5a9602c7ba3d5dbf2f
alsoox <alsoox173@gmail.com>
2025-02-25 14:11:50 +0900

    feat: SoundManager 추가

--- Assets/Scripts/UI/StrartSceneUI/SettingUI.cs
+++ Assets/Scripts/UI/StrartSceneUI/SettingUI.cs
@@ -5,18 +5,44 @@ using UnityEngine.UI;
 
 public class SettingUI : MonoBehaviour
 {
-    Button mainButton;
-
+    private Button mainButton;
+    private Slider soundSlider;
+    private Button muteButton;
+    private bool isMute = false;
+    private float prevVolume;
 
     void Awake()
     {
         mainButton = transform.Find("MainButton").GetComponent<Button>();
+        soundSlider = transform.Find("SoundSlider").GetComponent<Slider>();
+        muteButton = transform.Find("MuteButton").GetComponent<Button>();
 
+        muteButton.onClick.AddListener(OnClickMute);
         mainButton.onClick.AddListener(OnClickMainButton);
+        soundSlider.onValueChanged.AddListener(SetBackGruondSoundVolume);
+        
+    }
+
+    private void Start()
+    {
+        soundSlider.value = SoundManager.Instance.audioSource.volume;
+    }
+
+    void OnClickMute()
+    {
+        
+        SoundManager.Instance.audioSource.mute = !isMute;
+        isMute = !isMute;
+        
     }
 
     void OnClickMainButton()
     {
         this.gameObject.SetActive(false);
+    }
+
+    void SetBackGruondSoundVolume(float volume)
+    {
+        SoundManager.Instance.audioSource.volume = volume;
     }
 }
